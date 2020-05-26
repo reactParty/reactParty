39c082329335bf579a458d9f364bd5c01eab9760
olof <olof@takman.se>
2020-05-26 20:46:42 +0200

    fixade hover effect

--- my-app/src/components/Main.js
+++ my-app/src/components/Main.js
@@ -3,18 +3,39 @@ import Logo1 from './layout/Drinks.png';
 import Logo2 from './layout/Music.png';
 
 class Main extends Component {
+    increaseSize(event) {
+        event.target.style.height = "320px"
+        event.target.style.margin = "40px 75px"
+        event.target.style.cursor = "pointer"
+    }
+
+    decreaseSize(event) {
+        event.target.style.height = "300px"
+        event.target.style.margin = "50px 85px"
+        event.target.style.cursor = "none"
+    }
+
     render() {
         return (
             <main style={mainStyle}>
-                <img src={Logo1} alt="Logo" style={logoStyle1}/>
-                <img src={Logo2} alt="Logo" style={logoStyle2}/>                  
+                <img
+                    src={Logo1}
+                    alt="Logo"
+                    style={logoStyle1}
+                    onMouseOver={this.increaseSize}
+                    onMouseLeave={this.decreaseSize}/>
+                <img
+                    src={Logo2}
+                    alt="Logo"
+                    style={logoStyle2}
+                    onMouseOver={this.increaseSize}
+                    onMouseLeave={this.decreaseSize}/>
             </main>
         )
     }
 }
 
 const mainStyle = {
-    height: "500px",
     backgroundColor: "#fee",
     margin: "auto",
     textAlign: "center",
@@ -22,12 +43,14 @@ const mainStyle = {
 
 const logoStyle1 = {
     height: "300px",
-    margin: "60px 60px"
+    margin: "50px 85px",
+    transition: "all 0.5s"
 }
 
 const logoStyle2 = {
     height: "300px",
-    margin: "60px 60px"
+    margin: "50px 85px",
+    transition: "all 0.5s"
 }
 
 export default Main;

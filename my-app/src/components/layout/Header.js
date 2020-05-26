15da8cd7f5ef451a119154184f947db3ba6c4023
Victor Persson <victor.persson27@gmail.com>
2020-05-26 14:05:46 +0200

    new logo

--- my-app/src/components/layout/Header.js
+++ my-app/src/components/layout/Header.js
@@ -4,18 +4,19 @@ import Logo from './logo2.png';
 function Header() {
     return (
         <header style={headerStyle}>
+            <h1>reactParty</h1>
     
         </header>
     )
 }
 
 const headerStyle = {
-    height: "400px",
-    width: "100%",
-    backgroundImage: "url(" + Logo + ")",
-    backgroundpostion: "center",
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
+    height: "200px",
+    backgroundColor: "#9e2e50",
+    textAlign: "center",
+    margin: "auto",
+    color: "white",
+    fontFamily: 'Monoton'
 }
 
 export default Header;

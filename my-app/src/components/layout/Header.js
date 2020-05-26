d6f187f4447c5c0bdf4573873c39e4c13dd5a8d6
olof <olof@takman.se>
2020-05-26 21:02:41 +0200

    header object-fit test

--- my-app/src/components/layout/Header.js
+++ my-app/src/components/layout/Header.js
@@ -4,7 +4,7 @@ import Logo from './logo2.png';
 function Header() {
     return (
         <header>
-            <img src={Logo} alt="logo" width="100%"/>
+            <img style={{maxHeight: "400px", objectFit: "cover"}} src={Logo} alt="logo" width="100%"/>
         </header>
     )
 }

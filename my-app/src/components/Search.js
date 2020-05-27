03cd21e3e6ec495d6c92f6ed1026565a77dfc16b
Erica Sjoberg <ericasjoberg@Ericas-MacBook-Air.local>
2020-05-27 11:43:18 +0200

    searchform + logo

--- my-app/src/components/Search.js
+++ my-app/src/components/Search.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
+import searchLogo from './layout/search1.png';
 
 class Search extends Component {
     render() {
         return (
-            <div>
+            <div style={divStyle}>
                 <input style={searchStyle} type="text" placeholder="Search..." />
+                <img 
+                    style={logoStyle}
+                    src={searchLogo}
+                    alt="logo"/>
             </div>
         )
     }
 }
 
+const divStyle = {
+    display: "flex",
+    justifyContent: "center",
+    paddingTop: "10px",
+    paddingBottom: "10px"
+}
+
 const searchStyle = {
-    width: "80%",
-    marginLeft: "20px"
+  width: "60%",
+  height: "50px",
+  fontSize: "50px",
+  boxShadow: "none"
+}
+
+const logoStyle = {
+    height:"50px"
 }
 
 export default Search;

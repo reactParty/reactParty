263851fd81c12108504c7d18d9277e953e721bf6
Erica Sjoberg <ericasjoberg@Ericas-MacBook-Air.local>
2020-05-27 15:09:30 +0200

    text for popup

--- my-app/src/components/Popup.js
+++ my-app/src/components/Popup.js
@@ -10,10 +10,18 @@ class Popup extends Component {
                 <img src={closingCross} alt="ClosingCross" onClick={this.props.closePopup} style={{height: "40px", paddingLeft: "8px"}}/>
                 <article>
                     <h2>
-                        Tjaaaaa
+                        Hi, welcome to reactParty!
                     </h2>
                     <p>
-                        tjeeeenaaaaaa
+                        This app opens up the oppertunity for you and your friends to have the perfect cocktail night. 
+                        Don't know what to mix?
+                        No problem - just klick on the drink icon and search for whatever liqour
+                        you have availible at home. 
+                        Don't have time to make the drinks right away? 
+                        Save your favorite cocktails to your personal home and make them whenever you've got time.
+                        Need some good tunes as well? We got that covered! The Spotify icon will start some good 
+                        party music for ya'll!
+                        Have fun and drink responsibly.
                     </p>
                 </article>
             </div>  

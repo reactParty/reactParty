38df7fe392f11d858b8be75e55aa2c246dfe6dc2
Erica Sjoberg <ericasjoberg@Ericas-MacBook-Air.local>
2020-05-27 11:01:06 +0200

    searchfield to drinks

--- my-app/src/components/Search.js
+++ my-app/src/components/Search.js
@@ -3,9 +3,16 @@ import React, { Component } from 'react';
 class Search extends Component {
     render() {
         return (
-            <input />
+            <div>
+                <input style={searchStyle} type="text" placeholder="Search..." />
+            </div>
         )
     }
+}
+
+const searchStyle = {
+    width: "80%",
+    marginLeft: "20px"
 }
 
 export default Search;

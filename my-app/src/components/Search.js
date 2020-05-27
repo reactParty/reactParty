de5749ec1f7b6d1d787be94b082328a9c850dc7a
olof <olof@takman.se>
2020-05-27 10:39:00 +0200

    lagt till search

---
+++ my-app/src/components/Search.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+    render() {
+        return (
+            <input />
+        )
+    }
+}
+
+export default Search;

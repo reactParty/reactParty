68e5606a505a276afd8fa7fba74729bf6aee16db
Victor Persson <victor.persson27@gmail.com>
2020-05-28 15:08:45 +0200

    bilder till musikspelare

--- my-app/src/components/SpotifyPopUp.js
+++ my-app/src/components/SpotifyPopUp.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import closingCross from './layout/close.png'
+import playbtn from './layout/playbtn.png'
+import pausebtn from './layout/pausebtn.png'
+import nextsongbtn from './layout/nextsongbtn.png'
+import previousbtn from './layout/previousbtn.png'
 
 class Spotifypopup extends Component {
     render() {
         return(
             <div style={spotifyPopUp}> 
             <div style={SpotifyPopUpInner}> 
-            <img src={closingCross} alt="ClosingCross" onClick={this.props.closeSpotifyPopUp} style={{height: "50px", padding: "8px", cursor: "pointer"}}/>
+                <img src={closingCross} alt="ClosingCross" onClick={this.props.closeSpotifyPopUp} style={{height: "50px", padding: "8px", cursor: "pointer"}}/>
+                <div>
+                <img src={previousbtn} alt="previousbutton" />
+                <img src={playbtn} alt="playbutton" />
+                <img src={pausebtn} alt="pausebutton" />
+                <img src={nextsongbtn} alt="nextsongbutton" />
+
+                </div>
             </div>
             </div> 
         );

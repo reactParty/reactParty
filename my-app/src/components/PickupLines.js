0edc11db4c4e8918d8526f399603c6813e19042a
48591881 <lukas.lj95@gmail.com>
2020-06-02 11:29:09 +0200

    Kommenterade PickupLines

--- my-app/src/components/PickupLines.js
+++ my-app/src/components/PickupLines.js
@@ -3,18 +3,25 @@ import PickupLine from './PickupLine';
 import { Carousel } from 'react-responsive-carousel';
 
 class PickupLines extends Component {
+    /**
+     * 
+     * @param {*} props 
+     */
     constructor(props) {
         super(props);
-
         this.state = {
             currentSlide: 0
         };
     }
 
+    /**
+     * Render pickuplines in React Bootstrap Carousel
+     * @returns JSX 
+     */
     render() {
-        if (this.props.pickupLines.length === 0) return <div></div>;
-        setTimeout(() => { if ( this.state.currentSlide === 0) this.setState({currentSlide: 1}) }, 1000)
-        return (
+        if (this.props.pickupLines.length === 0) return <div></div>; // Creates an empty div elememt if pickuplines is empty
+        setTimeout(() => { if ( this.state.currentSlide === 0) this.setState({currentSlide: 1}) }, 1000) // Sets timer for pickupline
+        return ( // Returns the Bootstrap Carousel with set attributes
             <Carousel
                 showArrows={false}
                 selectedItem={this.state.currentSlide}
@@ -26,7 +33,7 @@ class PickupLines extends Component {
                 transitionTime={10000}
                 interval={10000}
                 showThumbs={false}>
-                {this.props.pickupLines.filter(pickupLine => pickupLine.tweet != null).map(
+                {this.props.pickupLines.filter(pickupLine => pickupLine.tweet != null).map( {/** Maps through pickuplines */}
                     (pickupLine, i) => {
                         return (
                             <div key={"pickupLineparent" + i} style={carouselItemStyle}>
@@ -40,15 +47,12 @@ class PickupLines extends Component {
     }
 }
 
+// Styles the div containing a pickupline
 const carouselItemStyle = {
     backgroundColor: "white",
     height: "66px",
     borderBottom: "5px black",
     borderBottomStyle: "double"
 }
-
-// const pickupLineStyle = {
-//     width: "100%"
-// }
 
 export default PickupLines;
